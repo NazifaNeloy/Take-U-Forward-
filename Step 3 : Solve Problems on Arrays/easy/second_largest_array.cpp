f2e4e29c105a1920249e4fa7e8ab254cedc33c6b
NazifaNeloy <241569685+NazifaNeloy@users.noreply.github.com>
2026-01-25 18:21:00 +0600

    added a better approch of the second largest element in the array

--- Step 3 : Solve Problems on Arrays/easy/second_largest_array.cpp
+++ Step 3 : Solve Problems on Arrays/easy/second_largest_array.cpp
@@ -1,12 +1,63 @@
+//better approach 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int largestnumber(vector<int>a , int n){
+int largest = a[0];
+for(int i=1;i<n;i++){
+if(a[i]>largest){
+largest =a[i];
+}
+}
+return largest;
+}
+
+
+int secondLargest(vector<int> &a, int n) {
+     int result_1 =largestnumber(a ,n);
+
+  cout<<"largest element is : "<<result_1 <<endl;
+    int largest = result_1;
+    int secondLargest = -1;
+    for(int i=n-2;i>0;i--){
+        if(a[i] > secondLargest && a[i]!=largest){
+            secondLargest = a[i];
+
+        }
+    }
+    return secondLargest;
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cout<<"enter the elemenets of array "<<i+1<<":";
+        cin>>a[i];
+    }
+    int result_2= secondLargest(a,n);
+    cout<<"The second largest element in the array is: "<<result_2<<endl;
+    return 0;
+}
+/*
+
+//brute force approach
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int secondLargest(vector<int> &a, int n) {
-    int largest = a(n-1);
+    int largest = a[n-1];
+    int secondLargest = -1;
     for(int i=n-2;i>0;i--){
         if(a[i]!=largest){
-            int secondLargest = a[i];
+            secondLargest = a[i];
 
         }
     }
@@ -15,14 +66,17 @@ int secondLargest(vector<int> &a, int n) {
 
 int main(){
     int n;
-    cout<,"Enter the number of elements in the array: ";
+    cout<<"Enter the number of elements in the array: ";
     cin>>n;
-    
+    vector<int>a(n);
     for(int i=0;i<n;i++){
-        cout<,"enter the elemenets pf array "<<i+1<<":";
+        cout<<"enter the elemenets of array "<<i+1<<":";
         cin>>a[i];
     }
     int result = secondLargest(a,n);
     cout<<"The second largest element in the array is: "<<result<<endl;
     return 0;
 }
+    
+*/
+
