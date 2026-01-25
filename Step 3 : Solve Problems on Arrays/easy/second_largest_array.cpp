c89932f86d22d030e14eb19fa48eed475c3281f7
NazifaNeloy <241569685+NazifaNeloy@users.noreply.github.com>
2026-01-25 19:03:36 +0600

    added a optimal approch of the second largest element in the array

--- Step 3 : Solve Problems on Arrays/easy/second_largest_array.cpp
+++ Step 3 : Solve Problems on Arrays/easy/second_largest_array.cpp
@@ -1,3 +1,55 @@
+//optimal approach 
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int secondLargest(vector<int> &a, int n) {
+    int largestnumber=a[0];
+    int secondLargest=-1;
+    for(int i=1;i<n;i++){
+        if(a[i]>largestnumber){
+            secondLargest=largestnumber;
+            largestnumber=a[i];
+        }
+        else if(a[i]<largestnumber && a[i]>secondLargest){
+            secondLargest=a[i];
+        }
+    }
+    return secondLargest;
+}
+int secondsmallest(vector<int> &a, int n) {
+    int smallestnumber=a[0];
+    int secondsmallest=INT_MAX;
+    for(int i=1;i<n;i++){
+        if(a[i]<smallestnumber){
+            secondsmallest=smallestnumber;
+            smallestnumber=a[i];
+        }
+        else if(a[i]>smallestnumber && a[i]<secondsmallest){
+            secondsmallest=a[i];
+        }
+    }
+    return secondsmallest;
+}
+     
+int main(){
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cout<<"enter the elemenets of array "<<i+1<<":";
+        cin>>a[i];
+    }
+    int result_2= secondLargest(a,n);
+    cout<<"The second largest element in the array is: "<<result_2<<endl;
+    return 0;
+}
+
+
+/*
 //better approach 
 #include <iostream>
 #include <vector>
@@ -44,7 +96,7 @@ int main(){
     cout<<"The second largest element in the array is: "<<result_2<<endl;
     return 0;
 }
-/*
+
 
 //brute force approach
 
