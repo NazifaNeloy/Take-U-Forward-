c28c020170c6b37b90057887d4980e6cad4e60f8
NazifaNeloy <241569685+NazifaNeloy@users.noreply.github.com>
2026-01-27 20:53:37 +0600

    remove duplicates from a sorted array in place

--- Step 3 : Solve Problems on Arrays/easy/check_if_sorted.cpp
+++ Step 3 : Solve Problems on Arrays/easy/check_if_sorted.cpp
@@ -5,9 +5,9 @@ int main() {
     int a[5] = {1, 5, 3, 7, 2};
 
     for (int i = 1; i < 5; i++) {
-        if (a[i] > a[i - 1]) {
-            cout << i << ", ";
-        } else {
+        if (a[i] >= a[i - 1]) {
+        } 
+        else {
             return false;
         }
     }
