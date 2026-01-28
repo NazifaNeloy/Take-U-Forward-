25f72d712149eae1b2210c2adb9dcec7d833c735
NazifaNeloy <241569685+NazifaNeloy@users.noreply.github.com>
2026-01-28 17:26:59 +0600

    Count digits of a number

--- step 1 :Learn the basics/Basic Maths /ExtractionOfDigits.cpp
+++ step 1 :Learn the basics/Basic Maths /ExtractionOfDigits.cpp
@@ -6,8 +6,31 @@ int main(){
     cin>>n;
     while(n>0){
         int last_digit = n%10;
-        cout<<last_digit;
+        cout<<last_digit<<endl;
         n=n/10; 
     }
     return 0;
 }
+
+/*
+
+//count digits 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count =0;
+    while(n>0){
+        int last_digit = n%10;
+       count = count +1;
+        n=n/10; 
+    }
+
+    cout<<count;
+    return 0;
+}
+
+*/
